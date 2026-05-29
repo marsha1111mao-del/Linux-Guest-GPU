e0e960df8a04e3f33bab14d624d2ddd61632242d
MZH-Work <mzh@google.com>
2026-05-29 21:52:24 +0800

    rm vanthor

--- drivers/vanthor/vanthor.h
+++
@@ -1,9 +0,0 @@
-#define DRV_NAME "vanthor"
-
-struct vanthor_device {
-	struct device *dev;
-	/** @phys_addr: Physical address of the iomem region. */
-	phys_addr_t phys_addr;
-	/** @iomem: CPU mapping of the IOMEM region. */
-	void __iomem *iomem;
-};
